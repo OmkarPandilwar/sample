1ddf0dde671e05ab85babb91adfb03ca6ac86c68
OmkarPandilwar <omkar.pandilwar06@gmail.com>
2026-02-11 09:31:40 +0530

    Assignment

---
+++ Garbagecolle/MyConsoleApp/Resource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Demo
+{
+    public class Resource
+    {
+        public string Name { get; set; }
+
+        public Resource(string name)
+        {
+            Name = name;
+            Console.WriteLine($"{Name} created");
+        }
+
+        ~Resource()
+        {
+            Console.WriteLine($"{Name} destroyed by GC");
+        }
+    }
+}
---
+++ Mydeligate/MyDeligate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DelegatesDemo
+{
+    class Program1
+    {
+        delegate void MyDelegate1();   // declared delegate
+
+        static void Hello()
+        {
+            Console.WriteLine("Hello from system");
+        }
+
+
+            static void Bye()
+        {
+            Console.WriteLine(" Bye from system .");
+        }
+
+            static void  Greet()
+        {
+            Console.WriteLine("Good night");
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            MyDelegate1 md = Hello; // method assigned to delegate
+            md+=Bye;
+            md+=Greet;
+            md();                     // invoke delegate
+        }
+    }
+}
